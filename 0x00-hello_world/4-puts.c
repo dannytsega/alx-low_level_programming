9cf67d3f70ea833d0331088d88e52b96aff102af
dannytsega <dannytsega16@gmail.com>
2025-07-06 04:17:41 -0400

    feat: add a program that displays a string using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the main function of the program
+ *
+ * Return: 0 if program succeeds
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
