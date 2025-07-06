a001a82ac785568fc341b31d5bb79c2cd12e53c2
dannytsega <dannytsega16@gmail.com>
2025-07-06 04:39:52 -0400

    feat: add a program that prints string to the console via printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function to use printf
+ *
+ * Return: 0 if program success
+ */
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
